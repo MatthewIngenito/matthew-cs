637f8f91eaf73cff8ae1804a6fbb7eeef27b57d8
matthewingenito <115803955+MatthewIngenito@users.noreply.github.com>
2022-11-04 15:17:55 +0000

    remote save initial work

--- script.js
+++ script.js
@@ -7,11 +7,35 @@ function submitForm(name, hometown) {
   var guestlist = document.getElementById("guestlist");
   var n = document.getElementById("name");
   var h = document.getElementById("hometown");
+  //saveLocal(name, hometown);
+  saveRemote(name, hometown);
   guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li>";
   n.value = "";
   h.value = "";
   n.focus();
   return false;
+}
+
+function saveLocal(name, hometown) {
+   var currentguestlist = localStorage.getItem("guestlist");
+  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>");
+}
+
+function saveRemote(name, hometown) {
+   $.get("save.php",{"name":name,"hometown":hometown});
+}
+
+function checkStorageGB() {
+  var guestlist = document.getElementById("guestlist");
+  var currentguestlist = localStorage.getItem("guestlist");
+  guestlist.innerHTML = currentguestlist;
+}
+
+function clearStorageGB() {
+  //var guestlist = document.getElementById("guestlist");
+  //guestlist.innerHTML = "";
+  $("#guestlist").html("The guestlist has been cleared.");
+  localStorage.setItem("guestlist","");
 }
 
 function checkStorage() {
